e081b1541efb0c9302b93b437672a07c3b210085
Bruno238932 <bruno.donadao@unesp.br>
2024-05-27 18:31:26 -0300

    Site versão 1

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Animação de carregamento do site
+    document.body.classList.add('loaded');
+    console.log("Site de R carregado com sucesso!");
+});
