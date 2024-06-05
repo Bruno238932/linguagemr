19d51546c50b39cc045788d0b028f8aff5a3da31
Bruno238932 <bruno.donadao@unesp.br>
2024-06-05 08:30:36 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,22 @@ document.addEventListener("DOMContentLoaded", function() {
     // Animação de carregamento do site
     document.body.classList.add('loaded');
     console.log("Site de R carregado com sucesso!");
+
+    // Botão Voltar ao Topo
+    const backToTopButton = document.getElementById('back-to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.pageYOffset > 100) {
+            backToTopButton.classList.add('show');
+        } else {
+            backToTopButton.classList.remove('show');
+        }
+    });
+
+    backToTopButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
 });
